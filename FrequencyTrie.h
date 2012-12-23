c17cb0e8ca67c09e6a6b574c12dd7914ea592812
Jay Thomason <jayt92@stanford.edu>
2012-12-22 22:26:04 -0800

    Finished early comments for header file without lookup methods.

--- FrequencyTrie.h
+++ FrequencyTrie.h
@@ -26,23 +26,77 @@ const size_t NUM_LETTERS = 26;
 /* Namespace */
 using namespace std;
 
+/* A class representing a frequency trie, which is simply 
+ * a prefix tree that also support frequency lookups
+ * for prefixes.
+ */
 class FrequencyTrie
 {
-  private:
-    struct node {
-      char ch;
-      size_t count;
-      bool isWord;
-      node *successors[NUM_LETTERS];
-    };
-    node *root;
-    node *createNode(const char ch);
-    void constructTree(string dictionaryFilename);
-    void insertWord(string word);
+private:
+  /* Struct: node
+   * Usage: node *myNode = new node;
+   *
+   * Struct representing a node in the trie. Each node
+   * includes a char for the prefix ending in that char,
+   * a frequency for the prefix ending in that char, a
+   * boolean representing whether or not the prefix 
+   * ending in that char is also a word in the dictionary,
+   * and an array of pointers to successor nodes.
+   */
+  struct node {
+    char ch;
+    size_t count;
+    bool isWord;
+    node *successors[NUM_LETTERS];
+  };
 
-  public:
-    FrequencyTrie(string dictionaryFilename);
-    ~FrequencyTrie();
+  node *root;
+
+  /* node *createNode(const char ch);
+   * Usage: node *newNode = createNode('a');
+   *
+   * Allocates memory for a new node and populates its
+   * fields accordingly. In particular the count is set
+   * to zero and the successor array is nulled out.
+   */
+  node *createNode(const char ch);
+  
+  /* void constructTree(string dictionaryFilename);
+   * Usage: constructTree("scrabbleDictionary.txt");
+   *
+   * Builds the trie from the supplied dictionary 
+   * text file. Note that it is essential that the
+   * dictionary file be formatted such that there
+   * is precisely one word per line and nothing else.
+   * Additionally, all words should either be purely
+   * lower-case, or purely upper-case
+   */
+  void constructTree(string dictionaryFilename);
+
+  /* void insertWord(string word);
+   * Usage: insertWord("tree");
+   *
+   * Inserts the given word into the trie, incrementing
+   * frequencies for prefixes as the trie is descended
+   * and creating new nodes when necessary.
+   */
+  void insertWord(string word);
+
+public:
+  /* Constructor: FrequencyTrie(string dictionaryFilename)
+   * Usage: FrequencyTrie trie("scrabbleDictionary.txt");
+   *
+   * Constructs a new FrequencyTrie from the dictionary
+   * text file supplied.
+   */
+  FrequencyTrie(string dictionaryFilename);
+
+  /* Destructor: ~FrequencyTrie()
+   * Usage: (implicit)
+   *
+   * Deallocates all memory used by the trie.
+   */
+  ~FrequencyTrie();
 };
 
 #endif
